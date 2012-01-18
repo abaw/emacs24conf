61b3b4b356935b69e25cf3af9d07c1c9230a320d
abaw <abawwu@gmail.com>
2012-01-19 00:53:56 +0800

    1. set user-emacs-directory to abaw/top-dir 2. added configurations for magit and yasnippet

--- init.el
+++ init.el
@@ -1,8 +1,7 @@
 (defvar abaw/top-dir (file-name-as-directory (file-name-directory load-file-name)))
 (defvar abaw/lisp-dir (file-name-as-directory (concat abaw/top-dir "lisp")))
 (add-to-list 'load-path abaw/lisp-dir)
+(setq user-emacs-directory abaw/top-dir)
 
 ;; FIXME: only tangle and compile file if .org has changed
 (org-babel-load-file (expand-file-name "config.org" abaw/top-dir))
-
-
