406e2000d35a3aed0fe02f6aa503321340a078f5
abaw <abawwu@gmail.com>
2012-01-21 09:05:10 +0800

    1. using color theme "zenburn"

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("69349beba557a65bb06f89b28b8fd2890c742f07" default)))
  '(org-src-fontify-natively t)
  '(yas/trigger-key "<C-tab>"))
 (custom-set-faces
@@ -10,4 +11,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Monaco")))))
+ '(default ((t (:stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :height 140 :foundry "apple" :family "Monaco")))))
