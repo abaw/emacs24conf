4b49ac850fd2c63248f61e1f98dace2087c093a3
abaw <abawwu@gmail.com>
2012-02-01 01:09:02 +0800

    1. changed the usage. I tend to use this as:  - create a symlink ~/.emacs.el to init.el  - directories such as elpa and el-get are created in ~/.emacs.d/..., so they won't mess up with current directory

--- init.el
+++ init.el
@@ -1,7 +1,18 @@
-(defvar abaw/top-dir (file-name-as-directory (file-name-directory load-file-name)))
+(require 'cl)
+
+(defun abaw/join-path (&rest components)
+  "Join path components into a path and return it. For
+  example, (abaw/join-path \"a\" \"b\" \"c\") returns \"a/b/c\"
+  in unix."
+  (when components
+    (if (cdr components)
+	(concat (file-name-as-directory (car components))
+		(apply 'abaw/join-path (cdr components)))
+      (car components))))
+
+(defvar abaw/top-dir (file-name-as-directory (file-name-directory (file-truename load-file-name))))
 (defvar abaw/lisp-dir (file-name-as-directory (concat abaw/top-dir "lisp")))
 (add-to-list 'load-path abaw/lisp-dir)
-(setq user-emacs-directory abaw/top-dir)
 
 ;; FIXME: only tangle and compile file if .org has changed
 (org-babel-load-file (expand-file-name "config.org" abaw/top-dir))
