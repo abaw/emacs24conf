af21ee56ab2e77f8fc3a1584ea748f2d52afe54a
Ken Wu <abaw@abaw-vpc.tw.video54.local>
2012-03-14 17:24:52 +0800

    1. put host related customization in per-host directory

---
+++ hosts/abaw-vpc/custom.el
@@ -0,0 +1,9 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("69349beba557a65bb06f89b28b8fd2890c742f07" default)))
+ '(org-src-fontify-natively t)
+ '(yas/trigger-key "<C-tab>"))
+
