74ed5bf01ce2545c2afa2f6c36a8f1dc4a2c9f62
Ken Wu <abawwu@gmail.com>
2013-03-13 15:07:25 +0800

    Add custom.el for abaw-cool

---
+++ hosts/abaw-cool/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(markdown-command "pandoc -f markdown -t html -s")
+ '(markdown-command-needs-filename nil)
+ '(yas-trigger-key "<C-tab>"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
