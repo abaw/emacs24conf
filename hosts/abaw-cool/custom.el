f4dfca36f154a2d2c290a428e02c9d8a2c6ad562
Ken Wu <abawwu@gmail.com>
2013-11-12 10:20:47 +0800

    Disable indent-tabs-mode for abaw-cool

--- hosts/abaw-cool/custom.el
+++ hosts/abaw-cool/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(indent-tabs-mode nil)
  '(markdown-command "pandoc -f markdown -t html -s")
  '(markdown-command-needs-filename nil)
  '(p4-auto-refresh nil)
